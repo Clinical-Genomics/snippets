ae159ba27ca9ced8d5b2bd877991ee0e566f030e
Kenny Billiau <kenny.billiau@scilifelab.se>
2015-12-10 10:32:31 +0100

    Feat rm old runs is is_oldrun_backuped.sh

--- is_oldrun_backuped.sh
+++ is_oldrun_backuped.sh
@@ -1,14 +1,20 @@
+#!/bin/bash
 # on rasta
 
 # To quickly check if a run in an oldRuns dir on a NAS is on tape already
 # 
 # 1/ generate a list on the NAS
-ssh clinical-nas-2 ls -1 oldRuns > clinical-nas-2-oldRuns
+OLDRUNS_FILE=$(mktemp)
+ssh $1 ls -1 oldRuns > $OLDRUNS_FILE
 
 # 2/ check if those are on tape
-while read -r LINE; do
-   echo $LINE;
+while read -r RUN; do
+   echo $RUN;
    
-   find /mnt/hds/proj/bioinfo/ON_TAPE/ -name "${LINE}*" | grep '.*'; FOUND=$?;
-   [[ $FOUND -eq 0 ]] && echo 'BACKED UP';
-done < clinical-nas-oldRuns 
+   find /mnt/hds/proj/bioinfo/ON_TAPE/ -name "${RUN}*" | grep '.*'; FOUND=$?;
+   if [[ $FOUND -eq 0 ]]; then
+       echo "ssh $1 'rm -Rf oldRuns/$RUN'"
+       ssh $1 "rm -Rf oldRuns/$RUN" &
+   fi
+done < $OLDRUNS_FILE
+rm $OLDRUNS_FILE
